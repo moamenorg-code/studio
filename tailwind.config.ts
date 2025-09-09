2f8b8ff2bca85a7a563d8dcfa3c21e46130b8636
مؤمن حسن <moamen.org@gmail.com>
2025-09-09 10:23:52 +0000

    استخدم خط كايرو

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['Cairo', 'sans-serif'],
+        headline: ['Cairo', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
