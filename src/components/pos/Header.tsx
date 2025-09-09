636bbe0452f609ed6a37a049a548a16b942064e6
مؤمن حسن <moamen.org@gmail.com>
2025-09-09 17:22:01 +0000

    لا تزال المشكله قائمه

--- src/components/pos/Header.tsx
+++ src/components/pos/Header.tsx
@@ -84,20 +84,18 @@ const Header: React.FC<HeaderProps> = ({
                   <span className="sr-only">{UI_TEXT.menu[language]}</span>
               </Button>
               </DropdownMenuTrigger>
-              <DropdownMenuContent align="start" className="w-[250px] p-0">
-                  <ScrollArea className="h-auto max-h-[70vh] p-1">
-                    {availableViews.map(({ value, label, icon: Icon }) => (
-                    <DropdownMenuItem key={value} onSelect={() => setActiveView(value)} className="py-2.5 text-base">
-                        <Icon className={language === 'ar' ? 'ms-3 h-5 w-5' : 'me-3 h-5 w-5'} />
-                        <span className="flex-1">{UI_TEXT[label][language]}</span>
-                    </DropdownMenuItem>
-                    ))}
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem onSelect={onLogout} className="py-2.5 text-base text-destructive">
-                        <LogOut className={language === 'ar' ? 'ms-3 h-5 w-5' : 'me-3 h-5 w-5'} />
-                        <span className="flex-1">{HEADER_UI_TEXT.logout[language]}</span>
-                    </DropdownMenuItem>
-                  </ScrollArea>
+              <DropdownMenuContent align="start" className="w-[250px] max-h-[80vh] overflow-y-auto">
+                {availableViews.map(({ value, label, icon: Icon }) => (
+                <DropdownMenuItem key={value} onSelect={() => setActiveView(value)} className="py-2.5 text-base">
+                    <Icon className={language === 'ar' ? 'ms-3 h-5 w-5' : 'me-3 h-5 w-5'} />
+                    <span className="flex-1">{UI_TEXT[label][language]}</span>
+                </DropdownMenuItem>
+                ))}
+                <DropdownMenuSeparator />
+                <DropdownMenuItem onSelect={onLogout} className="py-2.5 text-base text-destructive">
+                    <LogOut className={language === 'ar' ? 'ms-3 h-5 w-5' : 'me-3 h-5 w-5'} />
+                    <span className="flex-1">{HEADER_UI_TEXT.logout[language]}</span>
+                </DropdownMenuItem>
               </DropdownMenuContent>
           </DropdownMenu>
           <DropdownMenu>
