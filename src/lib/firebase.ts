317788d47562820b8a878a9e7ec013f8fdbdd711
مؤمن حسن <moamen.org@gmail.com>
2025-09-09 15:01:38 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  "projectId": "quickserve-lite-29vms",
+  "appId": "1:907702507941:web:7671fae8fe467b755fb04c",
+  "storageBucket": "quickserve-lite-29vms.firebasestorage.app",
+  "apiKey": "AIzaSyCKjl7xjLCCUHTbYlA5249L--OFPTuvclo",
+  "authDomain": "quickserve-lite-29vms.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "907702507941"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
