32493c6ccab9b029b67249839142e943656fc285
مؤمن حسن <moamen.org@gmail.com>
2025-09-10 09:25:09 +0000

    اعطيني اكواد النشر على فاير بيز

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -16,7 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  output: 'standalone'
 };
 
 export default nextConfig;
